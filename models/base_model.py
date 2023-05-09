c999254a65db116c6dcd66a2d6a0a685702a1391
Obianuju23 <obianujunmoh@gmail.com>
2023-05-09 15:13:25 -0700

    class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -17,13 +17,13 @@ class BaseModel:
     """initialization of the BaseModel class"""
         
         time_format =  %Y-%m-%dT%H:%M:%S.%f
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, time_form)
+                    self.__dict__[key] = datetime.strptime(value, time_format
                 else:
                     self.__dict__[key] = value
         else:
